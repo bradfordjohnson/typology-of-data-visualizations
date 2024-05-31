ce5f85f2e43946c1b102e7b66d6a08b2199da7c0
bradfordjohnson <bradfordljohnson21+github@gmail.com>
2024-05-30 23:59:27 -0400

    add box plot

---
+++ typology-of-data-visualizations/box.R
@@ -0,0 +1,56 @@
+library(ggplot2)
+library(dplyr)
+library(httpgd)
+library(sysfonts)
+library(showtext)
+
+showtext_auto()
+font_add_google("Urbanist", family = "Urbanist")
+
+font <- "Urbanist"
+background_color <- "white"
+plot_color <- "gray10"
+
+x <- rbinom(100, 12, .4)
+
+ggplot(, aes(x = x)) +
+  geom_boxplot(size = .5) +
+  geom_jitter(aes(y = 1)) +
+  theme_void() +
+  theme(
+    plot.background = element_rect(
+      fill = background_color,
+      color = background_color
+    ),
+    panel.background = element_rect(
+      fill = background_color,
+      color = background_color
+    ),
+    plot.margin = margin(1, 1, 1, 1, "cm"),
+    axis.line.x = element_line(size = .5),
+    axis.text.x = element_text(
+      size = 16,
+      family = font,
+      face = "bold",
+      color = plot_color,
+      margin = margin(2, 0, 0, 0, "mm")
+    ),
+    panel.grid.major.x = element_line(
+      color = plot_color,
+      size = .2,
+      linetype = 3,
+    ),
+    panel.grid.minor.x = element_line(
+      color = plot_color,
+      size = .2,
+      linetype = 3,
+    ),
+  )
+
+ggsave(
+  "typology-of-data-visualizations/visuals/box.png",
+  width = 4,
+  height = 4,
+  units = "in",
+  dpi = 300
+)
